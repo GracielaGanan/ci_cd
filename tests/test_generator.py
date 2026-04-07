17840f029a55caa01a9a662b63d995e81087f988
Graciela <graciela2401035@hybridge.education>
2026-04-07 11:27:25 -0300

    Update test_generator file

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -15,4 +15,19 @@ def test_password_is_string():
     assert isinstance(password, str)
 
 
-    
+def test_has_uppercase():
+    assert has_uppercase("Abc") is True
+
+
+def test_has_number():
+    assert has_number("abc1") is True
+
+
+def test_has_symbol():
+    assert has_symbol("abc!") is True
+
+
+def test_generated_password_length_range():
+    for length in range(4, 20):
+        password = generate_password(length)
+        assert len(password) == length
